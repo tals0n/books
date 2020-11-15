271d6095617a1c094e5cf16fa0e30e1270cab30f
tali <nico.kreutzer@kreuka.de>
2020-11-15 17:59:59 +0100

    ch17

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('anything-but-admin/, admin.site.urls),
+    path('anything-but-admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('pages.urls')),
     path('books/', include('books.urls')),
